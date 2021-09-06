004d0206c5b909ac9d438e1c51ef5bcf9aa82ac7
Alejandro Potosi <alejandropotosi@outlook.com>
2021-09-06 16:13:58 -0500

    Formas de iterar List y Set (Collection)

--- src/com/potoware/set/EjemploHashSetComparable.java
+++ src/com/potoware/set/EjemploHashSetComparable.java
@@ -2,13 +2,13 @@ package com.potoware.set;
 
 import com.potoware.set.modelo.Alumno;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class EjemploHashSetComparable {
     public static void main(String[] args) {
         Set<Alumno> sa = new HashSet<>();
+
+        //List<Alumno> sa = new LinkedList<>();
         sa.add(new Alumno("Juan",10));
         sa.add(new Alumno("Alexandre",10));
         sa.add(new Alumno("Pato",30));
@@ -17,5 +17,20 @@ public class EjemploHashSetComparable {
         sa.add(new Alumno("Armando",10));
         sa.add(new Alumno("Armando",4));
         System.out.println("sa = " + sa);
+
+        System.out.println("Iterando usando for each");
+        for(Alumno a: sa){
+            System.out.println(a);
+        }
+
+        System.out.println("Iterando usando while e iterator");
+        Iterator<Alumno> it = sa.iterator();
+
+        while(it.hasNext()){
+            Alumno a = it.next();
+            System.out.println(a);
+        }
+        System.out.println("Iterando usando stream forecch lamda");
+        sa.forEach(System.out::println);
     }
 }
